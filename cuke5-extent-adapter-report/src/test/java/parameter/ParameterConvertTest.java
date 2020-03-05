f8e3c810478eba52f67809461490fdb82a7cf045
grasshopper7 <grass.hopper.moc@gmail.com>
2020-03-05 21:44:10 +0530

    Updated cucumber jar version

--- cuke5-extent-adapter-report/src/test/java/parameter/ParameterConvertTest.java
+++ cuke5-extent-adapter-report/src/test/java/parameter/ParameterConvertTest.java
@@ -1,7 +1,11 @@
 package parameter;
 
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
+import org.junit.runner.RunWith;
 
-@CucumberOptions(plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"})
-public class ParameterConvertTest extends AbstractTestNGCucumberTests {}
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:" }, strict = true)
+public class ParameterConvertTest {
+}
