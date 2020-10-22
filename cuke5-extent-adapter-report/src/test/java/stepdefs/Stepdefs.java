81e68c04c039bbd451ed12143c49d351e42c801c
grasshopper7 <grass.hopper.moc@gmail.com>
2020-10-22 12:22:50 +0530

    Updated to 2.1.0

--- cuke5-extent-adapter-report/src/test/java/stepdefs/Stepdefs.java
+++ cuke5-extent-adapter-report/src/test/java/stepdefs/Stepdefs.java
@@ -1,6 +1,5 @@
 package stepdefs;
 
-
 import static org.junit.Assert.assertEquals;
 
 import java.util.List;
@@ -31,22 +30,69 @@ public class Stepdefs {
 
 	private Scenario scenario;
 
-	@Before
-	public void before() {
-		
+	@Before(value = "not @failure")
+	public void before(Scenario scenario) {
+		this.scenario = scenario;
+		scenario.write("BEFORE HI");
+		scenario.write("BEFORE HELLO");
 	}
-	
-	@After
-	public void after() {
-		
+
+	@After(value = "not @failure")
+	public void after(Scenario scenario) {
+
 	}
-	
+
+	@Before(value = "@failure")
+	public void beforeFailure(Scenario scenario) {
+		// System.out.println("Before failure");
+		this.scenario = scenario;
+		scenario.write("FAILURE HI");
+		scenario.write("FAILURE HELLO");
+		throw new RuntimeException();
+	}
+
+	@After(value = "@failure")
+	public void afterFailure() {
+		// System.out.println("After failure");
+		scenario.write("FAILURE HI");
+		scenario.write("FAILURE HELLO");
+		throw new RuntimeException();
+	}
+
+	@BeforeStep(value = "@failure")
+	public void beforeStepFailure() {
+		// System.out.println("Before Step failure");
+	}
+
+	@AfterStep(value = "@failure")
+	public void afterStepFailure() {
+		// System.out.println("After Step failure");
+	}
+
+	@Given("Hook failure step")
+	public void hook_failure_step() throws InterruptedException {
+		// System.out.println("Failure step");
+		scenario.write("FAILURE STEP HI");
+		scenario.write("FAILURE STEP HELLO");
+		Thread.sleep(500);
+	}
+
+	@Given("Skip hook failure step")
+	public void skip_hook_failure_step() throws InterruptedException {
+		Thread.sleep(250);
+	}
+
+	@Given("{string} background")
+	public void background(String type) {
+		System.out.format("%s type background. \n", type);
+	}
+
 	@Given("Write a {string} step with precondition in {string}")
 	@When("Complete action in {string} step in {string}")
 	@Then("Validate the outcome in {string} step in {string}")
 	public void step(String step, String scenario) throws InterruptedException {
 		System.out.format("%s step from %s.\n", step.toUpperCase(), scenario.toUpperCase());
-		Thread.sleep(500);
+		Thread.sleep(400);
 	}
 
 	@Then("Raise exception")
@@ -79,8 +125,8 @@ public class Stepdefs {
 	public void visitweb(String site) throws Exception {
 		driver.get(site);
 		this.site = site;
-		scenario.write("scenario write");
-		//scenario.log("scenario website name - " + site);
+		// scenario.write("scenario write");
+		scenario.write("scenario website name - " + site);
 		Thread.sleep(3000);
 	}
 
@@ -90,15 +136,16 @@ public class Stepdefs {
 		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
 		driver.manage().window().maximize();
+		scenario.write("HELLO THERE!!!");
 	}
 
 	@AfterStep(value = "@website")
 	public void afterSite() {
 		TakesScreenshot ts = (TakesScreenshot) driver;
 		byte[] screenshot = ts.getScreenshotAs(OutputType.BYTES);
-		scenario.embed(screenshot, "image/png", this.site);
+		// scenario.embed(screenshot, "image/png", this.site);
 		// scenario.embed(screenshot, "image/png");
-		//scenario.attach(screenshot, "image/png", this.site);
+		scenario.embed(screenshot, "image/png", this.site);
 		driver.quit();
 	}
 }
--- cuke5-extent-adapter-report/src/test/resources/stepdefs/datatable-docstring.feature
+++ cuke5-extent-adapter-report/src/test/resources/stepdefs/datatable-docstring.feature
@@ -1,15 +1,19 @@
 @tabledoc
 Feature: DataTable And DocString
-	
-	
+  DataTable And DocString Description
+
+  Background: 
+    Given 'Hello' background
+
+  @datatable
   Scenario: DataTable Scenario
     Given Customer orders the dishes
       | dish            | quantity |
       | Spicy Meatballs |        2 |
       | Fried Gnocchi   |        2 |
       | Espresso        |        3 |
-      
 
+  @docstring
   Scenario: Doc String to Custom Object
     Given the doc string is
       """
@@ -19,5 +23,4 @@ Feature: DataTable And DocString
       Whats new?
       
       Nothing much.
-      """      
-
+      """
