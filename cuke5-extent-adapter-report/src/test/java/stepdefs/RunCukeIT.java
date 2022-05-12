477aefeea4180a8c42618f3ef90e89a0ea5c9160
grasshopper7 <grass.hopper.moc@gmail.com>
2022-05-12 12:50:39 +0530

    Updated version

--- cuke5-extent-adapter-report/src/test/java/stepdefs/RunCukeIT.java
+++ cuke5-extent-adapter-report/src/test/java/stepdefs/RunCukeIT.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+@CucumberOptions(features = "src/test/resources/stepdefs/",plugin = { "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 		"summary" }, /* tags = "@both", */ strict = false)
 public class RunCukeIT extends AbstractTestNGCucumberTests {
 
